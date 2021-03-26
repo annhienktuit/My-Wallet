4c6d5ae5a623db0e97e5dae02ae4ad70ac28dbe6
annhienktuit <19520207@gm.uit.edu.vn>
2021-03-26 20:35:25 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # My Wallet
  Wallet money management app for SE114.L21 project
+
+ Ngôn ngữ: Kotlin
+ Coding Convention: [theo hướng dẫn này](https://kotlinlang.org/docs/coding-conventions.html)
+ 
---
+++ app/src/main/java/com/annhienktuit/mywallet/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.annhienktuit.mywallet
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_NO
+        ); //turn off night mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.setStatusBarColor(Color.parseColor("#FFFFFF"))
+        }
+        imageViewLogo.alpha = 0f
+        textViewTitle.alpha = 0f
+        imageViewLogo.animate().setDuration(1500).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+        textViewTitle.animate().setDuration(1500).alpha(1f)
+    }
+}
