6372ca947d6f8fdbf87045c11d14ebd3ff866536
annhienktuit <19520207@gm.uit.edu.vn>
2021-04-21 16:41:04 +0700

    fix bottom app bar

--- app/src/main/java/com/annhienktuit/mywallet/MainActivity.kt
+++ app/src/main/java/com/annhienktuit/mywallet/MainActivity.kt
@@ -6,19 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
-import androidx.appcompat.app.AppCompatDelegate
-import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCompatDelegate.setDefaultNightMode(
-                AppCompatDelegate.MODE_NIGHT_NO
-        ); //turn off night mode
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val window: Window = window
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.setStatusBarColor(Color.parseColor("#FFFFFF"))
+            window.setStatusBarColor(Color.parseColor("#4CAF50"))
         }
     }
+
 }
