a8326a0383e199873b9cf5b01dff7b5125fe6058
HinTran013 <HinTran013@users.noreply.github.com>
2021-04-22 07:26:43 +0700

    complete bottom app bar

--- app/src/main/java/com/annhienktuit/mywallet/MainActivity.kt
+++ app/src/main/java/com/annhienktuit/mywallet/MainActivity.kt
@@ -7,17 +7,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide();
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val window: Window = window
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.setStatusBarColor(Color.parseColor("#4CAF50"))
+            window.statusBarColor = Color.parseColor("#4CAF50")
         }
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 
 }
