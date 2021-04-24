1e3788a88a94ffc6b1f295f11ec02d95e877f2b1
HinTran013 <HinTran013@users.noreply.github.com>
2021-04-24 10:39:29 +0700

    add transition behavior for 2 child fragments in report fragment

--- app/src/main/java/com/annhienktuit/mywallet/fragments/ReportFragment.kt
+++ app/src/main/java/com/annhienktuit/mywallet/fragments/ReportFragment.kt
@@ -1,14 +1,16 @@
 package com.annhienktuit.mywallet.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.annhienktuit.mywallet.R
 import com.annhienktuit.mywallet.adapter.SelectionPagerAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import kotlinx.android.synthetic.main.fragment_report.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,18 +36,21 @@ class ReportFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_report, container, false)
     }
 
-    //call onActivityCreated method
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+         viewPager.adapter = SelectionPagerAdapter(childFragmentManager)
+         tabLayout.setupWithViewPager(viewPager)
     }
 
     companion object {
