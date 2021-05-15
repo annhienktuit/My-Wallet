96a80556b856427bc5c847609af203111a90da5c
Khai Hoan <khaihoan2306@gmail.com>
2021-05-15 21:14:05 +0700

    Add section Managing Cards (ATM, Credit) in list

---
+++ app/src/main/java/com/annhienktuit/mywallet/CardActivity.kt
@@ -0,0 +1,11 @@
+package com.annhienktuit.mywallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card)
+    }
+}
---
+++ app/src/main/java/com/annhienktuit/mywallet/adapter/CardAdapter.kt
@@ -0,0 +1,33 @@
+package com.annhienktuit.mywallet.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.annhienktuit.mywallet.R
+import com.annhienktuit.mywallet.`object`.Card
+import kotlinx.android.synthetic.main.layout_card_planning.view.*
+
+class CardAdapter(private val cardList: List<Card>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val card: TextView = itemView.cardName
+        val person: TextView = itemView.personName
+        val bank: TextView = itemView.bankName
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.layout_card_planning, parent, false)
+        return CardViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentItem = cardList[position]
+        var holder = holder as CardViewHolder
+        holder.card.text = currentItem.nameCard
+        holder.person.text = currentItem.namePerson
+        holder.bank.text = currentItem.nameBanking
+    }
+
+    override fun getItemCount() = cardList.size
+}
