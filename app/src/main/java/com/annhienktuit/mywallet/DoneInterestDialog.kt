af933330ddd7e6de6be9087c19681580fa9469a3
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-05-18 10:32:38 +0700

    completely pass data

--- app/src/main/java/com/annhienktuit/mywallet/DoneInterestDialog.kt
+++ app/src/main/java/com/annhienktuit/mywallet/DoneInterestDialog.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 
 class DoneInterestDialog: DialogFragment() {
@@ -12,6 +13,14 @@ class DoneInterestDialog: DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dialog_done_interest_rate, container, false)
+        val rootView: View = inflater.inflate(R.layout.dialog_done_interest_rate, container, false)
+
+        val closeBtn: Button = rootView.findViewById(R.id.closeBtn) as Button
+
+        closeBtn.setOnClickListener{
+            dismiss()
+        }
+
+        return rootView
     }
 }
