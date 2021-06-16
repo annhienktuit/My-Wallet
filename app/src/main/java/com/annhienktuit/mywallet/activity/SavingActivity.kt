6fce120045af42581192f45a35fbb65339d8827f
Le Khai Hoan <72954994+khaihoan2306@users.noreply.github.com>
2021-06-16 13:29:55 +0700

    Update SavingActivity.kt

--- app/src/main/java/com/annhienktuit/mywallet/activity/SavingActivity.kt
+++ app/src/main/java/com/annhienktuit/mywallet/activity/SavingActivity.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.EditText
